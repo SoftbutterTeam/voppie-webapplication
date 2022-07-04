49b46016430b45a6b7aee54c8a430be685208cc4
emcleandev <58271203+emcleandev@users.noreply.github.com>
2022-07-04 11:58:00 +0100

    init

---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/main.css";
+import "./styles/tailwind.css";
+import App from "./App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <div className="h-full bg-gray-50">
+      <div className="h-full">
+        <App />
+      </div>
+    </div>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
---
+++ src/routes.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Navigate, Route, Routes } from "react-router";
+
+function AppRoutes() {
+  return <Routes></Routes>;
+}
+
+export default AppRoutes;
