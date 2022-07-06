0e9c7fdd1c828c08d99cecb9a50ac3a61dad2bf7
Emmanuel Mclean <emclean.dev@gmail.com>
2022-07-06 09:35:01 +0100

    login page v.0.1

--- src/ui/layouts/page-layout/fullAuth.layout.tsx
+++ src/ui/layouts/page-layout/fullAuth.layout.tsx
@@ -4,11 +4,13 @@ interface Props {
   children: React.ReactElement;
 }
 export function FullAuthLayout(props: Props) {
+  const { children: mainContent } = props;
+
   return (
     <PrimBackground>
       <div className="grid grid-cols-1 sm:grid-cols-10 h-screen w-full">
         <div className=" flex flex-col justify-center items-center sm:col-span-6">
-          <h1>fgfddfg</h1>
+          {mainContent}
         </div>
         <div className="hidden sm:block sm:col-span-4">sdfdsf</div>
       </div>
--- src/ui/pages/authentication/login/login.page.tsx
+++ src/ui/pages/authentication/login/login.page.tsx
@@ -5,8 +5,14 @@ export function Login() {
   return (
     <FullAuthLayout>
       <div className="w-full flex flex-col justify-center items-center">
-        <div className="py-12 px-4 sm:px-6 lg:px-8 max-w-md  space-y-8">fd</div>
+        <div className="py-12 px-4 sm:px-6 lg:px-8 max-w-md  space-y-8">
+          <LoginPanel />
+        </div>
       </div>
     </FullAuthLayout>
   );
+}
+
+function LoginPanel() {
+  return <>abbcc</>;
 }
