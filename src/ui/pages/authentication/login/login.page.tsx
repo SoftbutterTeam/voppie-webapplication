a189ebee3ae820d43af9750d393478d29689bf85
Emmanuel Mclean <emclean.dev@gmail.com>
2022-07-29 13:25:05 +0100

    +

--- src/ui/pages/authentication/login/login.page.tsx
+++ src/ui/pages/authentication/login/login.page.tsx
@@ -67,7 +67,7 @@ function OrDivide({ text = "or" }: Props2) {
     <div className="w-full flex py-2 items-center">
       <div className="bg-gray-200 shadow h-[1px] w-full ml-2" />
       <p className="mx-2 text-slate-500 text-sm">{text}</p>
-      <div className="bg-gray-200 shadow h-[1px] w-full ml-2" />
+      <div className="bg-gray-200 shadow h-[1px] w-full mr-2" />
     </div>
   );
 }
